78c130e6e5f4bc0eb2f019bc420188fe43bdca90
Chia Yun Cheng <yuncc13@gmail.com>
2017-10-11 12:32:42 +0800

    modify main.cpp

--- main.cpp
+++ main.cpp
@@ -8,11 +8,6 @@ cout<<"Hello World"<<endl;
 cout<<"Hotflix -Add few more endl";
 cout<<endl<<endl;
 
-
-cout<<"Working on branch"<<endl;
-
-
-cout<<"This will create merge conflict"<<endl;
 return 0;
 
 
